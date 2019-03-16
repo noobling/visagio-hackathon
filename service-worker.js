8295d5e435cc1c5f143396c19af311dc2a74332d
David Yu <david.yu@readify.net>
2019-03-16 13:01:39 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/visagio-hackathon/precache-manifest.cc71ad85d98672502932d10649f90341.js"
+  "/visagio-hackathon/precache-manifest.06a1c10c63b9704767f6629c95fb683c.js"
 );
 
 workbox.clientsClaim();
